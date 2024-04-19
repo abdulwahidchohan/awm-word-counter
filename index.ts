5d0313f5aa3d812d7f960db06cba2c370fb49a33
abdulwahidchohan <91005376+abdulwahidchohan@users.noreply.github.com>
2024-04-19 19:50:43 +0500

    Make a word counter

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers: {
+    Sentence: string
+} = await inquirer.prompt([
+    {
+        type: "input",
+        name: "Sentence",
+        message: "Enter a sentence"
+    }
+])
+const words = answers.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Your words length is ${words.length}`);
